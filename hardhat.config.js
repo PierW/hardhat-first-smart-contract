a22ee050f470f17f398213665d2cf3f2801138b2
Pierpaolo Wurzburger <pierpaolo.wurzburger@gmail.com>
2022-01-05 19:16:22 +0100

    inizio configurazione testnet + Rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.0",
+  networks : {
+    rinkeby : {
+      url : 'https://eth-rinkeby.alchemyapi.io/v2/E6JezYqaZW2jdDBEkKVroXGBZXizULAJ',
+      accounts : '',
+    }
+  },
 };
